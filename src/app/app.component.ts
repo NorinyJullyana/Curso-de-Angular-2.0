c841bcfd83dd9ca6ee269a4f3826a57af4d30f05
Noriny Jullyana <noriny.pessoa@gmail.com>
2021-12-29 15:17:08 -0300

    Imagem e título do Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+  title = 'Neko Girls';
+  url = 'https://external-preview.redd.it/7jotHTAXg5XC56M9EtpTUPzGiwW8Vr1cZkRmudDHdkY.png?width=801&auto=webp&s=02f9fa6ee02c1d7d691890464e957e4efb034d87';
+  description = 'Chocola';
 }
